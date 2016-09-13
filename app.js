548ecbf435d23b7136a2dca75781c6a1afcea234
Xavier Crochet <xacrochet@gmail.com>
2016-09-13 14:17:56 +0200

    updated mongodb variable name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var Client = require('ssh2-sftp-client');
 var sftp = new Client();
 
-db.connect(process.env.MONGOLAB_RED_URI);
+db.connect(process.env.MONGODB_URI);
 app.listen(process.env.PORT || 3000 );
 
 module.exports = sftp;
